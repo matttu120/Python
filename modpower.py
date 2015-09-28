64c3800a124fd12537a3cb950350424962b43b24
Matt Tu <matttu120@gmail.com>
2015-09-27 17:50:37 -0700

    Create modpower.py

---
+++ modpower.py
@@ -0,0 +1,13 @@
+'''
+Task 
+You are given three integers; print two lines. 
+The first line should print pow(a,b), and the second line should print the result of pow(a,b,m).
+'''
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+
+a = int(raw_input())
+b = int(raw_input())
+m = int(raw_input())
+
+print pow(a,b)
+print pow(a,b,m)
